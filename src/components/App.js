b20ac246c10ec685ca9ec1459be1f2b257f5c048
Leon <leon.muriuki@student.moringaschool.com>
2023-12-05 21:24:19 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch a random dog image from the API
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => {
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching dog image:", error);
+        setLoading(false);
+      });
+  }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+  return (
+    <div>
+      <h1>Random Dog Image</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" style={{ maxWidth: "100%" }} />
+      )}
+    </div>
+  );
+}
+
+export default App;
